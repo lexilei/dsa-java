ba9020694e6b153ac158d89583a7fcbd0e7e7320
lexilei <zhihan.lei@emory.edu>
2023-01-30 17:37:15 -0500

    quiz1 first attemp

---
+++ src/main/java/edu/emory/cs/algebraic/LongIntegerQuiz.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 Emory University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.emory.cs.algebraic;
+import java.util.Arrays;
+
+/** @author Jinho D. Choi */
+public class LongIntegerQuiz extends LongInteger {
+    public LongIntegerQuiz(LongInteger n) { super(n); }
+
+    public LongIntegerQuiz(String n) { super(n); }
+
+    @Override
+    protected void addDifferentSign(LongInteger n) {
+        int big= compareAbs(n);
+        if (big==0){
+            digits=new byte[1];
+            return;
+        }
+        //determine sign
+        if (big>0&&isPositive() || big<0&&n.isPositive()){
+            sign=Sign.POSITIVE;
+        }else{
+            sign=Sign.NEGATIVE;
+        }
+        //carry out calculation
+        //first create result array
+        int m = Math.max(digits.length, n.digits.length);
+        byte[] result = new byte[m+1];
+        //if big>0 copy this and subtract n
+        if (big>0) {
+            System.arraycopy(digits, 0, result, 0, digits.length);
+            for (int i = 0; i < m; i++) {
+                if (i < n.digits.length)
+                    result[i] -= n.digits[i];
+                if (result[i] < 0) {
+                    result[i] += 10;
+                    result[i + 1] -= 1;
+                }
+            }
+            //else copy n subtract this
+        }else{
+            System.arraycopy(n.digits, 0, result, 0, n.digits.length);
+            for (int i = 0; i < m; i++) {
+                if (i < digits.length)
+                    result[i] -= digits[i];
+                if (result[i] < 0) {
+                    result[i] += 10;
+                    result[i + 1] -= 1;
+                }
+            }
+        }
+
+        //now get rid off zeros at the front
+        while (result[m]==0&&m>0){
+            m-=1;
+        }
+
+
+        digits = Arrays.copyOf(result, m+1);
+    }
+}
---
+++ src/test/java/edu/emory/cs/algebraic/LongIntegerQuizTest.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2020 Emory University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.emory.cs.algebraic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/** @author Jinho D. Choi */
+public class LongIntegerQuizTest {
+    @Test
+    public void test0() {
+        LongInteger a = new LongIntegerQuiz("123");
+        a.add(new LongIntegerQuiz("0"));
+        assertEquals("123", a.toString());
+    }
+
+    @Test
+    public void test2() {
+        LongInteger a = new LongIntegerQuiz("123");
+        a.add(new LongIntegerQuiz("-0"));
+        assertEquals("123", a.toString());
+    }
+
+    @Test
+    public void test3() {
+        LongInteger a = new LongIntegerQuiz("123");
+        a.add(new LongIntegerQuiz("-123"));
+        assertEquals("0", a.toString());
+    }
+
+    @Test
+    public void test4() {
+        LongInteger a = new LongIntegerQuiz("-123");
+        a.add(new LongIntegerQuiz("123"));
+        assertEquals("-0", a.toString());
+    }
+
+    @Test
+    public void test5() {
+        LongInteger a = new LongIntegerQuiz("123");
+        a.add(new LongIntegerQuiz("-122"));
+        assertEquals("1", a.toString());
+    }
+
+    @Test
+    public void test6() {
+        LongInteger a = new LongIntegerQuiz("-123");
+        a.add(new LongIntegerQuiz("122"));
+        assertEquals("-1", a.toString());
+    }
+
+    @Test
+    public void test7() {
+        LongInteger a = new LongIntegerQuiz("123");
+        a.add(new LongIntegerQuiz("-124"));
+        assertEquals("-1", a.toString());
+    }
+
+    @Test
+    public void test8() {
+        LongInteger a = new LongIntegerQuiz("-123");
+        a.add(new LongIntegerQuiz("124"));
+        assertEquals("1", a.toString());
+    }
+
+    @Test
+    public void test9() {
+        LongInteger a = new LongIntegerQuiz("123");
+        a.add(new LongIntegerQuiz("-45678"));
+        assertEquals("-45555", a.toString());
+    }
+
+    @Test
+    public void test10() {
+        LongInteger a = new LongIntegerQuiz("-12345");
+        a.add(new LongIntegerQuiz("678"));
+        assertEquals("-11667", a.toString());
+    }
+    @Test
+    public void test11() {
+        LongInteger a = new LongIntegerQuiz("0");
+        a.add(new LongIntegerQuiz("-12"));
+        assertEquals("-12", a.toString());
+    }
+    @Test
+    public void test12() {
+        LongInteger a = new LongIntegerQuiz("11");
+        a.add(new LongIntegerQuiz("-12"));
+        assertEquals("-1", a.toString());
+    }
+    @Test
+    public void test13() {
+        LongInteger a = new LongIntegerQuiz("1");
+        a.add(new LongIntegerQuiz("-12"));
+        assertEquals("-11", a.toString());
+    }
+    @Test
+    public void test14() {
+        LongInteger a = new LongIntegerQuiz("0");
+        a.add(new LongIntegerQuiz("-0"));
+        assertEquals("0", a.toString());
+    }
+    @Test
+    public void test15() {
+        LongInteger a = new LongIntegerQuiz("-0");
+        a.add(new LongIntegerQuiz("0"));
+        assertEquals("-0", a.toString());
+    }
+}
