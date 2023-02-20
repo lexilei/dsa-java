c30ad37ee88117283c548b2c7118657262f0dcdd
lexilei <zhihan.lei@emory.edu>
2023-02-19 23:29:57 -0500

    okay so now the code works just need to benchmark them

--- src/main/java/edu/emory/cs/sort/distribution/RadixSortQuiz.java
+++ src/main/java/edu/emory/cs/sort/distribution/RadixSortQuiz.java
@@ -30,55 +30,34 @@ public class RadixSortQuiz extends RadixSort {
         int maxBit = getMaxBit(array, beginIndex, endIndex);
         bitsort(array, beginIndex, endIndex, maxBit);
     }
-    private static void bitsort(Integer[] array, int beginIndex, int endIndex, int b) {
+    private void bitsort(Integer[] array, int beginIndex, int endIndex, int b) {
         if (beginIndex >= endIndex || b <= 0) {
             return;
         }
 
         int[] count = new int[10];
-//
+
         for (int i = beginIndex; i < endIndex; i++) {
             int num = array[i] / (int) Math.pow(10, b-1);
             count[num%10]++;
-            System.out.println(array[i]);
         }
 
         for (int i = 1; i < 10; i++) {
             count[i] += count[i - 1];
         }
 
+        //now sort the array
+        int div = (int)Math.pow(10, b-1);
+        sort(array, beginIndex, endIndex, key -> (key / div) % 10);
 
-        int[] sorted = new int[endIndex - beginIndex ];
-
-        // sort elements based on the current digit
-        for (int i = endIndex-1; i >= beginIndex; i--) {
-            int num = (int)(array[i-1] / Math.pow(10, b-1));
-            sorted[count[num%10-1]-2] = array[i-1];
-            count[num%10]++;
-        }
-
-        // copy the sorted elements back to the original array
-        for (int i = beginIndex; i < endIndex; i++) {
-            array[i] = sorted[i-beginIndex];
-            System.out.println(array[i]);
-        }
-
+        int nextLeft =beginIndex;
+        int nextRight =beginIndex;
         // recursively sort elements based on the next digit
-        for (int i = 1; i <10; i++) {
-            int nextLeft = beginIndex + count[i-1];
-            int nextRight = beginIndex + count[i] ;
-            System.out.println("next up:"+nextLeft+" "+nextRight);
+        for (int i = 0; i <10; i++) {
+            if (i>0 && count[i] == count[i-1]) continue;
+            nextLeft = nextRight;
+            nextRight = beginIndex + count[i] ;
             bitsort(array, nextLeft, nextRight, b - 1);
-        }
-    }
-
-
-    public static void main(String[] args){
-        Integer[] test=new Integer[]{14,26,99,90,96,20};
-        RadixSortQuiz test2=new RadixSortQuiz();
-        test2.sort(test,0,test.length);
-        for (int i=0;i<test.length;i++){
-            System.out.print(test[i]+",");
         }
     }
 }
