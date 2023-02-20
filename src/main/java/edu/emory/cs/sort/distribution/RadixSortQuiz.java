f8c694186b49f9d383d41a27b94eb3c5f5cd0619
lexilei <zhihan.lei@emory.edu>
2023-02-19 21:55:04 -0500

    have no idea what's going on at this point but gonna let my laptop cool for a bit, and my brain

--- src/main/java/edu/emory/cs/sort/distribution/RadixSortQuiz.java
+++ src/main/java/edu/emory/cs/sort/distribution/RadixSortQuiz.java
@@ -31,46 +31,49 @@ public class RadixSortQuiz extends RadixSort {
         bitsort(array, beginIndex, endIndex, maxBit);
     }
     private static void bitsort(Integer[] array, int beginIndex, int endIndex, int b) {
-        if (beginIndex >= endIndex || b < 0) {
+        if (beginIndex >= endIndex || b <= 0) {
             return;
         }
 
         int[] count = new int[10];
 
         for (int i = beginIndex; i < endIndex; i++) {
-            int num = array[i] / (int) Math.pow(10, b);
-            count[num % 10]++;
+            int num = array[i] / (int) Math.pow(10, b-1);
+            count[num%10]++;
         }
 
         for (int i = 1; i < 10; i++) {
             count[i] += count[i - 1];
+            System.out.println(count[i]);
         }
+
 
         int[] sorted = new int[endIndex - beginIndex + 1];
 
         // sort elements based on the current digit
         for (int i = endIndex-1; i >= beginIndex; i--) {
-            int num = array[i] / (int) Math.pow(10, b);
-            sorted[count[num % 10] - 1] = array[i];
-            count[num % 10]--;
+            int num = (int)(array[i] / Math.pow(10, b-1))-1;
+            sorted[count[num%10]] = array[i];
+            count[num%10]++;
         }
 
         // copy the sorted elements back to the original array
         for (int i = beginIndex; i < endIndex; i++) {
-            array[i] = sorted[i - beginIndex];
+            array[i] = sorted[i-beginIndex];
         }
 
         // recursively sort elements based on the next digit
-        for (int i = 1; i < 10; i++) {
-            int nextLeft = beginIndex + count[i - 1];
+        for (int i = 1; i <10; i++) {
+            int nextLeft = beginIndex + count[i-1];
             int nextRight = beginIndex + count[i] ;
+            System.out.println("next up:"+nextLeft+" "+nextRight);
             bitsort(array, nextLeft, nextRight, b - 1);
         }
     }
 
 
     public static void main(String[] args){
-        Integer[] test=new Integer[]{5,21,345,87,429,12084,32,1};
+        Integer[] test=new Integer[]{14,26,99,90,96,20};
         RadixSortQuiz test2=new RadixSortQuiz();
         test2.sort(test,0,test.length);
         for (int i=0;i<test.length;i++){
--- src/test/java/edu/emory/cs/sort/SortQuizTest.java
+++ src/test/java/edu/emory/cs/sort/SortQuizTest.java
@@ -20,16 +20,18 @@ import edu.emory.cs.sort.comparison.ShellSortQuiz;
 import edu.emory.cs.sort.distribution.LSDRadixSort;
 import edu.emory.cs.sort.distribution.RadixSort;
 import edu.emory.cs.sort.distribution.RadixSortQuiz;
+import org.junit.jupiter.api.Test;
 
 /** @author Jinho D. Choi */
 public class SortQuizTest extends SortTest {
+    @Test
     public void testRobustness() {
-        testRobustness(new ShellSortQuiz<>());
+        //testRobustness(new ShellSortQuiz<>());
         testRobustness(new RadixSortQuiz());
     }
-
+    @Test
     public void testRuntime() {
         testRuntime(new ShellSortKnuth<>(), new ShellSortQuiz<>());
-        testRuntime(new LSDRadixSort(), new RadixSortQuiz());
+        //testRuntime(new LSDRadixSort(), new RadixSortQuiz());
     }
 }
