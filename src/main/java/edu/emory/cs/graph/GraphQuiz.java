aa40141935cd1ead7d4fb9783b183d3ae1c8fba1
lexilei <zhihan.lei@emory.edu>
2023-04-02 23:55:29 -0400

    passed?? or still funky?

--- src/main/java/edu/emory/cs/graph/GraphQuiz.java
+++ src/main/java/edu/emory/cs/graph/GraphQuiz.java
@@ -15,83 +15,91 @@
  */
 package edu.emory.cs.graph;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /** @author Jinho D. Choi */
+
 public class GraphQuiz extends Graph {
-    public GraphQuiz(int size) { super(size); }
-    public GraphQuiz(Graph g) { super(g); }
+    public GraphQuiz(int size) {
+        super(size);
+    }
 
-    /** @return the total number of cycles in this graph. */
-    public int numberOfCycles() {
-        int count=0;
+    public GraphQuiz(Graph g) {
+        super(g);
+    }
+
+
+    public int numberOfCycles(){
+        // Create a deque of all nodes in the graph
         Deque<Integer> notVisited = IntStream.range(0, size()).boxed().collect(Collectors.toCollection(ArrayDeque::new));
-        Set<Integer> visited=new HashSet<>();
-        //count+=countingCycles(notVisited.poll(), new ArrayDeque<>(notVisited), new HashSet<>(visited),0);
-        while (!notVisited.isEmpty()) {
-            int target=notVisited.peek();
-            int a=count;
-            count+=countingCycles(notVisited.poll(), new ArrayDeque<>(notVisited), new HashSet<>(visited),target);
-            //visited.add(target);
-            System.out.println(target+" "+(count-a));
-        }
-        return count;
-    }
-    private int countingCycles(int target, Deque<Integer> notVisited, Set<Integer> visited,int parent) {
-        notVisited.remove(target);
-        //visited.add(target);
-        if (notVisited.isEmpty()) return 0;
-        int count=0;
+        // Get the first node in the deque
+        int i = notVisited.getFirst();
+        // Create a list of all points in the graph
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        HashSet<ArrayList<Integer>> all_trees = new HashSet<>();
+        ArrayList<Integer> points = IntStream.range(0, size()).boxed().collect(Collectors.toCollection(ArrayList::new));
 
-        for (Edge edge : getIncomingEdges(target)) {
-
-            int source = edge.getSource();
-            if (source == parent) count++;
-            else if (visited.contains(source)) continue;
-            else {
-                int a= countingCycles(source, notVisited, new HashSet<>(visited),parent);
-                count+=a;
+        // For each point in the graph, generate all possible paths from that point
+        for (int point: points){
+            ArrayList<Integer> start = new ArrayList<>();
+            start.add(point);
+            result.add(start);
+            while (result.size()!=0){
+                // Update the list of paths
+                result = updatePaths(result);
+                ArrayList<ArrayList<Integer>> result_copy = new ArrayList<>();
+                // For each path, check if it's a tree or a cycle
+                for (ArrayList<Integer> path: result){
+                    HashSet<Integer> hashSet = new HashSet<>(path);
+                    // If the path has a repeated node, it's a cycle
+                    if (hashSet.size() < path.size()){
+                        int n = path.indexOf(path.get(path.size()-1));
+                        List<Integer> temp = path.subList(n, path.size()-1);
+                        int y = temp.indexOf(Collections.min(temp));
+                        List<Integer> sorted = new ArrayList<>(temp.subList(y, temp.size()));
+                        List<Integer> list = new ArrayList<>(temp.subList(0, y));
+                        sorted.addAll(list);
+                        // hashSet = new HashSet<>(sorted);
+                        all_trees.add((ArrayList<Integer>) sorted);
+                    }
+                    else{
+                        // Otherwise, it's still a tree
+                        result_copy.add(path);
+                    }
+                }
+                result = result_copy;
             }
-
         }
-        return count;
+        return all_trees.size();
     }
 
-
-
-
-
-    public static void main(String[] args){
-        GraphQuiz g = new GraphQuiz(5);
-        g.setDirectedEdge(0, 1, 0);
-        g.setDirectedEdge(0, 2, 0);
-        g.setDirectedEdge(2, 1, 0);
-        g.setDirectedEdge(2, 3, 0);
-        g.setDirectedEdge(3, 4, 0);
-        g.setDirectedEdge(4, 2, 0);
-        System.out.println(g.numberOfCycles());//should be 1
-
-//        GraphQuiz g = new GraphQuiz(5);
-//        g.setDirectedEdge(0, 1, 0);
-//        g.setDirectedEdge(0, 2, 0);
-//        g.setDirectedEdge(2, 1, 0);
-//        g.setDirectedEdge(2, 3, 0);
-//        g.setDirectedEdge(3, 4, 0);
-//        g.setDirectedEdge(4, 2, 0);
-//        System.out.print(g.numberOfCycles());
-
-//        GraphQuiz g = new GraphQuiz(5);
-//        g.setDirectedEdge(0, 1, 0);
-//        g.setDirectedEdge(1, 2, 0);
-//        g.setDirectedEdge(1, 3, 0);
-//        g.setDirectedEdge(2, 0, 0);
-//        g.setDirectedEdge(3, 4, 0);
-//        g.setDirectedEdge(4, 2, 0);
-//        System.out.print(g.numberOfCycles());
+    // Given a list of paths, generates all possible paths by adding new nodes to the end of each path
+    public ArrayList<ArrayList<Integer>> updatePaths(ArrayList<ArrayList<Integer>> result){
+        int result_length = result.size();
+        ArrayList<ArrayList<Integer>> result_copy = new ArrayList<>();
+        for (int j = 0; j <= result.size()-1; j++){
+            int last = result.get(j).get(result.get(j).size()-1);
+            ArrayList<Edge> edges = (ArrayList<Edge>) this.getIncomingEdges(last);
+            if (edges.size() > 1) {
+                ArrayList<Integer> copied_lists = null;
+                for (int k = 0; k <= edges.size()-1; k++) {
+                    copied_lists = (ArrayList<Integer>) result.get(j).clone();
+                    copied_lists.add(edges.get(k).getSource());
+                    result_copy.add(copied_lists);
+                }
+            }
+            else{
+                if (edges.size() == 1) {
+                    ArrayList<Integer> copied_lists = null;
+                    copied_lists = (ArrayList<Integer>) result.get(j).clone();
+                    copied_lists.add(edges.get(0).getSource());
+                    result_copy.add(copied_lists);
+                }
+            }
+        }
+        result = result_copy;
+        return result;
     }
 }
